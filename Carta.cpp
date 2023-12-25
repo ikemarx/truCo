4567b73205a8adab666ee170a5a7b681e8f2c082
Henrique <69089960+ikemarx@users.noreply.github.com>
2023-12-24 21:15:48 -0400

    Update Carta.cpp

--- Carta.cpp
+++ Carta.cpp
@@ -1,4 +1,22 @@
 #include "Carta.h"
 
 Carta::Carta(int valor, int naipe) : valor(valor), naipe(naipe) {}
+
 Carta::~Carta() {}
+
+int Carta::getValor() {
+    return valor;
+}
+
+
+int Carta::getNaipe() {
+    return naipe;
+}
+
+void Carta::setValor(int valor) {
+    this->valor = valor;
+}
+
+int Carta::setNaipe() {
+    this->naipe = naipe;
+}
